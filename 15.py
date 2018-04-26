f7ea4bdc8b3fc98a707686c26c9297fea34550a5
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2018-04-25 20:19:55 -0400

    done

--- 15.py
+++ 15.py
@@ -1,13 +1,31 @@
-def pcheck(pword):
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXTYZ"
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    num = "0123456789"
-    upnum = [1 for x in pword if x in upper]
-    lownum = [1 for x in pword if x in lower]
-    numnum = [1 for x in pword if x in num]
+#Caleb Smith-Salzberg
+#SoftDev2 pd7
+#K15 -- Do You Even List?
+#2018-04-25
 
-    
-    
-    return (1 in upnum and 1 in lownum and 1 in numnum)
+import math
+
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXTYZ"
+lower = "abcdefghijklmnopqrstuvwxyz"
+num = "0123456789"
+special = ".?!&#,;:-_*"
+
+def pcheck(pword):
+    all = [1 if x in upper else 2 if x in lower else 3 if x in num else 0 for x in pword]
+    return (1 in all and 2 in all and 3 in all)
 
 print pcheck("Hell0")
+
+def prate(pword):
+    lengthPtsL = [.5 for x in pword]
+    if len(pword) > 12:
+        lengthPts = 6
+    else :
+        lengthPts = math.floor(len(lengthPtsL) * .5)
+    upperPts = 1 in [1 for x in pword if x in upper]
+    lowerPts = 1 in [1 for x in pword if x in lower]
+    numPts = 1 in [1 for x in pword if x in num]
+    specialPts = 1 in [1 for x in pword if x in special]
+    return lengthPts + upperPts + lowerPts + numPts + specialPts
+
+print prate("He9?")
